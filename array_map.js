ae82ec43d9d6e2b981759a53e1e35c9e65f7ed86
Joshua Cole <j_cole_356@yahoo.com>
2017-11-22 20:55:03 -0800

    use single quotes when possible in array_map

--- array_map.js
+++ array_map.js
@@ -5,7 +5,7 @@ function ArrayMap() {
 ArrayMap.prototype.get = function (key) {
   for (var i = 0; i < this.store.length; i++) {
     if (this.store[i][0] === key) {
-      return "{" + this.store[i][0] + " => " + this.store[i][1] + "}";
+      return '{' + this.store[i][0] + ' => ' + this.store[i][1] + '}';
     }
   }
 
@@ -17,12 +17,12 @@ ArrayMap.prototype.set = function (key, value) {
     for (var i = 0; i < this.store.length; i++) {
       if (this.store[i][0] === key) {
         this.store[i][1] = value;
-        return "{" + this.store[i][0] + " => " + this.store[i][1] + "}";
+        return '{' + this.store[i][0] + ' => ' + this.store[i][1] + '}';
       }
     }
   } else {
     this.store.push([key, value]);
-    return "{" + this.store[this.store.length - 1][0] + " => " +
-      this.store[this.store.length - 1][1] + "}";
+    return '{' + this.store[this.store.length - 1][0] + ' => ' +
+      this.store[this.store.length - 1][1] + '}';
   }
 };
